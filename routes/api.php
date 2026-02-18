7471d2906d638799b1d75967e5204498fb23499c
Akrati Gupta <akratigupta220504@gmail.com>
2026-02-18 15:33:14 +0530

    login api added

---
+++ routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
